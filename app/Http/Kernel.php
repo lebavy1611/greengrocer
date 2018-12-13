6afaf014366699e9408a20cf4dcbf4aea9da2600
at-vyle <vy.le@asiantech.vn>
2018-12-13 16:58:04 +0700

    SDSD

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,6 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
-            \Barryvdh\Cors\HandleCors::class,
         ],
     ];
 
@@ -62,7 +61,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'cors' => \Barryvdh\Cors\HandleCors::class,
+        'cors' => \App\Http\Middleware\Cors::class,
         'verify.admin' => \App\Http\Middleware\VerifyAdmin::class,
         'verify.user' => \App\Http\Middleware\VerifyUser::class,
     ];
